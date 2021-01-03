6416a250a48e26490e2290c3c2cd2befdab38f8f
Kalaiarasan Senthil <70711693+kalai-senthil@users.noreply.github.com>
2021-01-03 09:13:57 +0530

    Font Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App"
+import "./index.css"
+import "./ProductSansMedium.ttf"
+import "./ProductSansBold.ttf"
+import "./ProductSans.ttf"
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
